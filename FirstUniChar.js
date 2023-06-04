55db2381b845fd75e278b66d26503e07903ab6c7
ayesha31ash <43003827+ayesha31ash@users.noreply.github.com>
2023-06-04 23:30:06 +0530

    Add files via upload

---
+++ FirstUniChar.js
@@ -0,0 +1,23 @@
+function firstUniqChar(s) {
+  const charCount = new Map(); // Hashmap to store character frequencies
+  
+  // Count the frequency of each character
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    charCount.set(char, (charCount.get(char) || 0) + 1);
+  }
+  
+  // Find the index of the first non-repeating character
+  for (let i = 0; i < s.length; i++) {
+    if (charCount.get(s[i]) === 1) {
+      return i;
+    }
+  }
+  
+  return -1; // No non-repeating character found
+}
+
+// Example:
+console.log(firstUniqChar("leetcode")); // Output: 0
+console.log(firstUniqChar("loveleetcode")); // Output: 2
+console.log(firstUniqChar("aabb")); // Output: -1
